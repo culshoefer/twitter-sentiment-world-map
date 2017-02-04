078e1a53c0ab470122626c17d8802a47502d02fe
Anirudh <anirudhx5@gmail.com>
2017-02-04 17:46:08 +0000

    getting top trends for a place

--- flaskproj/twitter.py
+++ flaskproj/twitter.py
@@ -1,5 +1,6 @@
 import tweepy
 from envvars import *
+import json
 
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -7,6 +8,15 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def get_top_trends(woeid):
+    """
+    Gives the top trends for the location
+    Args:
+        :woeid is Yahoo woeid of the place
+    """
+    data = api.trends_place(698064)
+    result = []
+    for trend in data[0]["trends"]:
+        result.append(trend["name"])
+    return result
