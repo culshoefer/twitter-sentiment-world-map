70900a01abadaeac3af14bfd717bf11c5b46be7c
Anirudh <anirudhx5@gmail.com>
2017-02-11 21:17:09 +0000

    set up twitter conduit

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -1,6 +1,28 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import Web.Twitter.Conduit
+import Data.ByteString.Char8 (pack)
+import qualified Constants as C
+
+tokens :: OAuth
+tokens = twitterOAuth
+    { oauthConsumerKey = (pack C.oauthConsumerKey)
+    , oauthConsumerSecret = (pack C.oauthConsumerSecret)
+    }
+
+
+credential :: Credential
+credential = Credential
+    [ ((pack "oauth_token"), (pack C.oauthToken))
+    , ((pack "oauth_token_secret"), (pack C.oauthTokenSecret))
+    ]
+
+twInfo = setCredential tokens credential def
+
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    mgr <- newManager tlsManagerSettings
+    timeline <- call twInfo mgr homeTimeline
+    print timeline
+    return ()
